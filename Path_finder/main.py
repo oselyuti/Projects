f993cc8683ef2a4a4122a82457465b0a96052481
Alex Selyutin <oselyuti@vols.utk.edu>
2021-09-27 23:05:46 -0400

    working_version needs formatting

--- Path_finder/main.py
+++ Path_finder/main.py
@@ -30,11 +30,37 @@ def print_path (visited):
             print ((n.i, n.j) ,",", sep = '' , end = '')#add a comma between the tuples
         i+=1
     print (']', sep ='')
-    print ("Traversed_to_goal:", len(visited))
+    #print ("Traversed_to_goal:", len(visited))
+
+    def print_grid(grid):
+        for line in grid:
+            for i in line:
+                print (i, end=',')
+            print()
+
+
 
 
 
 class PathPlanner:
+
+    def print_info (result):
+
+        path_exists = False
+        visited = []
+        total_nodes_traversed =0
+        if (result[0] == True):
+                path_exists = True
+                visited = result[1]
+                total_nodes_traversed = result[2]
+        if path_exists:
+            print_path (visited)
+            print ("Traversed:", total_nodes_traversed)
+        else:
+            print("No path has been found to your goal node.")
+            sys.exit()
+
+
     def create_node_grid (filename, num_rows, num_cols):
         file = open(filename, 'r')
         row_index = 0
@@ -77,51 +103,48 @@ class PathPlanner:
                 if (n.j-1) >=0:
                     n.left = node_grid[n.i][n.j-1]
                 
-                
-                n.neighbors = [n.left, n.top, n.right, n.bottom]
-                if (search_type =="DFS"):
+                if search_type == "BFS":
+                    n.neighbors = [n.left, n.top, n.right, n.bottom]
+                if (search_type =="DFS" or search_type == "astar" or search_type == "ALL" ):
                     n.neighbors = [n.bottom,n.right,n.top,n.left]
 
-    def print_info (result):
-
-        path_exists = False
-        visited = []
-        total_nodes_traversed =0
-        if (result[0] == True):
-                path_exists = True
-                visited = result[1]
-                total_nodes_traversed = result[2]
         
+    def compute_dist_for_all (node_grid, goal_node,):
+        for line in node_grid:
+            for element in line:
+        #prepare for A*
+                list_of_cells = node_grid[element.i][element.j].neighbors
+                for i in range(4):
+                    loc = list_of_cells[i]
+                    if loc != None and loc.value == 0:# cell exists has distance can be calculated
+                        loc.distance_to_goal = math.sqrt( ((loc.i - goal_node.i)**2) + ((loc.j- goal_node.j)**2) )
 
-
-
-        if path_exists:
-            print_path (visited)
-            print ("Traversed:", total_nodes_traversed)
-        else:
-            print("No path has been found to your goal node.")
-            sys.exit()
+                #rearrange the list of neighbors for the cell n to reflect the order in which to try neighboring cells
+                #START STATE: # (bottom->right->top->left)                
 
                 
-
+        return node_grid
 
     def bfs(root, grid, node_grid, num_rows, num_cols,goal_node):
         found_path = False
+        if root == goal_node:
+            found_path = True
+
         queue = [root]
         traversed = 0
-        cur_node = root # just to initialize it
+        cur_node = root
 
-        # cur_node.i == goal_node.i and cur_node.j == goal_node.j) == False and
         while len(queue) > 0:
+            if (found_path):
+                break
             cur_node = queue.pop(0)
             cur_node.visited = 1
             node_grid[cur_node.i][cur_node.j] = cur_node
-            traversed += 1
-
             if (cur_node.i == goal_node.i and cur_node.j == goal_node.j) == True:
                 found_path= True
                 break
 
+            traversed += 1
             if (cur_node.i + 1) < num_rows:
                 #print ("try to go down given that we haven't visited the bottom from the top node")
                 if (grid[cur_node.i + 1][cur_node.j] == 0):  # free to go
@@ -182,34 +205,56 @@ class PathPlanner:
 
         return found_path, route, traversed
 
-                
+    def dfs (cell, visited, goal_node,found):
+        if cell:
+            visited.append (cell)
+            print ( (cell.i,cell.j),end ='')
+            if (cell.i == goal_node.i and cell.j == goal_node.j ):
+                found = True
+                cur_path = len(visited)
+                print (" ",cur_path )
+                return True
+            visited.append (cell)
+
+            #neighbor_copy.pop(0)
+            for i in range (4):
+                if i == 0:
+                    for neighbor in cell.neighbors:
+                        if neighbor not in visited :
+                                if (PathPlanner.dfs (neighbor, visited, goal_node, found)):
+                                        return True            
+            return False
 
 
+    """
+    
+    """
     def deepening_dfs (node_grid, root, goal_node):
         start = node_grid[root.i][root.j]
         cur_depth = 1
+        
         goal = False
         goal_i = goal_node.i; goal_j = goal_node.j
+        if (start.i == goal_i and start.j == goal_j):
+            goal = True
         
         list_of_neighbors_at_this_level = [start,goal_node]
         start.visited = 0
         start.prev = None
         nodes_traversed = 0
-        absolute_shortest_path = math.inf
-        cur_shortest_path = math.inf
 
         while (1):  
             cur_depth+=1
             if goal==True:
                 break
-            
-
-            for individual in list_of_neighbors_at_this_level:
+            while (len(list_of_neighbors_at_this_level)>0):
+                individual = list_of_neighbors_at_this_level[0]
                 individual.visited = 1
+                nodes_traversed+=1
+                
                 #get their list of adjacent cells
                 if (individual and individual.value ==0):
                     for i in individual.neighbors:
-                        
                         if i :
                             nodes_traversed+=1
                             if i.visited == 0 and i.value != 1:
@@ -217,10 +262,9 @@ class PathPlanner:
                                 i.visited = 1
                                 i.prev = individual
                                 if i.i == goal_i and i.j == goal_j:
-                                    #print ("found\n")
                                     goal=True
-                                    #break
-            list_of_neighbors_at_this_level.remove(individual)#make sure only the new connections are in the list
+                list_of_neighbors_at_this_level.remove(individual)#make sure only the new connections are in the list
+
 
         #prepare info for printing
         n = node_grid[goal_node.i][goal_node.j]
@@ -231,141 +275,70 @@ class PathPlanner:
         route.reverse()
 
         return goal, route, nodes_traversed    
-
-
-    def astar(node_grid,root, goal_node):#DOES SOME WEIRD SHIT 0,0 TO 5,4 GRID2
-        visited = []
-        found_path = False
-        traversed=0
-        n = root #index use only
-        stack = [node_grid[root.i][root.j] ]
-        prev_dist = math.inf
-
-        while len(stack) > 0:
-            n = stack.pop()
-            n.visited = 1
-            traversed+=1
-            if n not in visited:
-                visited.append(n)
-            if (n.i == goal_node.i and n.j == goal_node.j):
-                found_path = True
-                break
-
-            #prepare for A*
-            better_distance = [True,True,True,True]
-            list_of_cells = node_grid[n.i][n.j].neighbors
-            for i in range(4):
-
-                loc = list_of_cells[i]
-                if loc != None and loc.visited == 0 and loc.value == 0:# cell exists has distance can be calculated
-                    loc.distance_to_goal = math.sqrt( ((loc.i - goal_node.i)**2) + ((loc.j- goal_node.j)**2) )
-                    if (loc.prev and loc.distance_to_goal > loc.prev.distance_to_goal):#not improving
-                        better_distance[i] = False # better distance not found
-                        print ("TRASH... child:", loc.i,loc.j, "parent is ", loc.prev.i, loc.prev.j)
-
-            at_least_one_better_distance_found = True
-            count_of_larger_distances =0
-            for i in range(4):
-                if better_distance [i] == False:
-                    count_of_larger_distances+=1
-            if count_of_larger_distances == 3: # no better distance is found from this node n
-                at_least_one_better_distance_found = False
-                print ("NONE CAN BE REACHED")
-
-            if at_least_one_better_distance_found == False:
-                n.value = 1 #consider this an obstacle in the future
-
-
-
-                
-            #rearrange the list of neighbors for the cell n to reflect the order in which to try neighboring cells
-            #START STATE: left,top,right, bottom
-            for i in range(4):
-                low_priority = n.neighbors[i]
-                if low_priority: #check whether the neighbor is within the grid
-                    for k in range(4):
-                        higher_priority = n.neighbors[k]
-                        if higher_priority:
-                            if (low_priority.distance_to_goal < higher_priority.distance_to_goal and i<k):#if there is more than 1 optimal path, check the priority
-                                #the node with higher priority will have a higher index
-                                #swap places if the node at the start of the list has lesser distance to goal than the one at the end(bottom, for example)
-                                #the element with the lowest priority and the lowest distance then bubbles up to the top of the neighbor list
-                                tmp = higher_priority
-                                n.neighbors[k] = low_priority
-                                n.neighbors[i] = tmp
-    
             
-            
-            list_of_cells = node_grid[n.i][n.j].neighbors
-            #the cells to be explored in order of the smallest (g+f) first, meaning they will pushed on stack last
-            list_of_cells = node_grid[n.i][n.j].neighbors
-            for i in list_of_cells:
-                if (i and i.visited == 0 and i.value ==0):
-                    stack.append(i)
-                    i.prev = n
 
-        return found_path, visited, traversed
-            
+    """
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    def deepening_dfs_astar (node_grid, root, goal_node):
+    """
+    def deepening_dfs_astar (node_grid,root, goal_node):
         start = node_grid[root.i][root.j]
-        cur_depth = 1
         goal = False
         goal_i = goal_node.i; goal_j = goal_node.j
+        if (start.i == goal_i and start.j == goal_j):
+            goal = True
         
-        list_of_neighbors_at_this_level = [start,goal_node]
-        start.visited = 0
-        start.prev = None
+        list_of_neighbors_at_this_level = [start]
         nodes_traversed = 0
-        absolute_shortest_path = math.inf
-        cur_shortest_path = math.inf
+        cur_lowest_distance_to_goal = 99999
+
+        node_grid = PathPlanner.compute_dist_for_all (node_grid, goal_node)
 
         while (1):  
-            cur_depth+=1
             if goal==True:
                 break
-            
-
-            for individual in list_of_neighbors_at_this_level:
+            #for individual in list_of_neighbors_at_this_level:
+            while (len(list_of_neighbors_at_this_level)>0):
+                if goal==True:
+                    break
+                
+                individual = list_of_neighbors_at_this_level[0]
                 individual.visited = 1
+                nodes_traversed+=1
+
                 #get their list of adjacent cells
                 if (individual and individual.value ==0):
-                    for i in individual.neighbors:
-                        
+                     #ADD SOME STUFF TO COME OUT OF IT IF GOING NOWHERE
+                    not_better=0
+                    for s in individual.neighbors:
+                        if (s and s.distance_to_goal > cur_lowest_distance_to_goal ):
+                            not_better+=1
+
+                    if not_better==4:
+                        individual.value = 1
+                        cur_lowest_distance_to_goal = individual.prev.distance_to_goal
+                        if individual.prev:
+                            cur_lowest_distance_to_goal = individual.prev.distance_to_goal
+                            list_of_neighbors_at_this_level.remove(individual)
+                            list_of_neighbors_at_this_level.insert(0, individual.prev)
+                            individual = individual.prev                            
+
+
+                    for i in individual.neighbors: 
                         if i :
-                            nodes_traversed+=1
-                            if i.visited == 0 and i.value != 1:
-                                i.distance_to_goal = math.sqrt( ((i.i - goal_node.i)**2) + ((i.j- goal_node.j)**2) )
-
-
-
-
-
-                                list_of_neighbors_at_this_level.append(i)
-
-
-
-
+                            if i.visited == 0 and i.value != 1 :
                                 i.visited = 1
                                 i.prev = individual
-                                if i.i == goal_i and i.j == goal_j:
-                                    #print ("found\n")
-                                    goal=True
-                                    #break
-            list_of_neighbors_at_this_level.remove(individual)#make sure only the new connections are in the list
 
+                                if i.distance_to_goal >= cur_lowest_distance_to_goal:
+                                    list_of_neighbors_at_this_level.append(i)
+                                elif i.distance_to_goal <= cur_lowest_distance_to_goal :
+                                    #print ("(Improving distance)",i.i,i.j, i.distance_to_goal)
+                                    cur_lowest_distance_to_goal = i.distance_to_goal
+                                    list_of_neighbors_at_this_level.insert(0,i)
+                                    if i.i == goal_i and i.j == goal_j:
+                                        goal=True  
+                list_of_neighbors_at_this_level.remove(individual)#make sure only the new connections are in the list
+        
         #prepare info for printing
         n = node_grid[goal_node.i][goal_node.j]
         route = []
@@ -379,7 +352,9 @@ class PathPlanner:
 
 
 
-
+"""
+    
+"""
 def main(argv):
     filename = ''
     start_node = ''
@@ -407,10 +382,6 @@ def main(argv):
             print("Usage: main.py --input -start --goal --search")
             sys.exit(3)
 
-
-
-
-
     num_rows = 0
     num_cols = 0
     file = open(filename, 'r')
@@ -424,16 +395,13 @@ def main(argv):
                 sys.exit()
     file.close()
 
-
     s = start_node.split(',')
     if (s[0].isdigit() == False or int(s[0]) >= num_rows or (s[1].isdigit() == False or int(s[1]) >= num_cols)):
         print("Start coordinate is invalid or is beyond the size of the grid")
         sys.exit()
-    # use s[0],s[1] to set the root
     root = node(int(s[0],), int(s[1]))
 
     s = goal_node.split(',')
-    # create a class instance with (int)s[0],s[1]
     if (s[0].isdigit() == False or int(s[0]) >= num_rows or (s[1].isdigit() == False or int(s[1]) >= num_cols)):
         print("Goal coordinate is invalid or is beyond the size of the grid")
         sys.exit()
@@ -441,14 +409,7 @@ def main(argv):
 
 
 
-
-
-
-
-
-
     #Start working with a class
-    
     node_grid, grid = PathPlanner.create_node_grid (filename, num_rows, num_cols)# create a grid
     # Consider a case when a goal_node is an obstacle. The program then gracefully quits
     if (grid[goal_node.i][goal_node.j]) == 1:
@@ -457,52 +418,42 @@ def main(argv):
 
 
 
+    #PathPlanner.print_grid(grid)
+
     #initialize all of the grid nodes to know about their neighbors
     PathPlanner.initialize_node_grid (node_grid, num_rows, num_cols, search_type)
-            
-        
     if search_type == "BFS":
         result = PathPlanner.bfs(root, grid, node_grid, num_rows, num_cols,goal_node)
-    #start dfs
     elif search_type == "DFS":
-        result = PathPlanner.deepening_dfs(node_grid, root, goal_node)
-    
+        result = PathPlanner.deepening_dfs(node_grid, root, goal_node)        
     elif search_type == "astar": #g(n) = 1 for all, so I will discard it
-        result = PathPlanner.astar(node_grid,root,goal_node)
-    
+        result = PathPlanner.deepening_dfs_astar(node_grid,root,goal_node)
+ 
+
+
     elif search_type == "ALL":
         #first one is ready to go
-        result = PathPlanner.bfs(root, grid, node_grid, num_rows, num_cols,goal_node)
-        PathPlanner.print_info(result)
+        result_bfs = PathPlanner.bfs(root, grid, node_grid, num_rows, num_cols,goal_node)
 
-
-        #second needs a newly made grid with no modifications
+        #second need a newly made grid with no modifications
         node_grid, grid = PathPlanner.create_node_grid (filename, num_rows, num_cols)# create a grid
-        #initialize all of the grid nodes to know about their neighbors
-        PathPlanner.initialize_node_grid (node_grid, num_rows, num_cols, search_type)
+        PathPlanner.initialize_node_grid (node_grid, num_rows, num_cols, search_type)#initialize all of the grid nodes to know about their neighbors
+        result_dfs = PathPlanner.deepening_dfs(node_grid, root, goal_node)
 
-        result = PathPlanner.deepening_dfs(node_grid, root, goal_node)
-        PathPlanner.print_info(result)
-
-
-
-
-
-        #second needs a newly made grid with no modifications
         node_grid, grid = PathPlanner.create_node_grid (filename, num_rows, num_cols)# create a grid
-        #initialize all of the grid nodes to know about their neighbors
         PathPlanner.initialize_node_grid (node_grid, num_rows, num_cols, search_type)
-        result = PathPlanner.astar(node_grid,root,goal_node)
-        PathPlanner.print_info(result)
+        result_astar = PathPlanner.deepening_dfs_astar(node_grid, root,goal_node)
+        results = [result_bfs, result_dfs, result_astar]
 
+        for r in results:
+            PathPlanner.print_info (r)
     else:
         print ("Unknown search type has been specified")
         sys.exit()
-        
 
     if search_type != "ALL":
-        PathPlanner.print_info (result)
-
+        PathPlanner.print_info(result)
+        print ()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
